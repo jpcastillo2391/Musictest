2aaaaed404d87c0d6ca2f10be3c7eb2c2377bbed
jose castillo msi laptop <josecastillo.uvg@gmail.com>
2019-05-10 17:11:19 -0600

    puta

--- MusicFileFolderSite/MusicSite/urls.py
+++ MusicFileFolderSite/MusicSite/urls.py
@@ -1,14 +1,25 @@
 from django.conf.urls import url, include
 from . import views
 
+
+
 app_name = 'musicss'
 urlpatterns = [
 
-    url(r'^$', views.index, name='musicsite'),
+    url(r'^$', views.IndexView.as_view(), name='musicsite'),
     # aqui se declara algum_id
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detalles'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detalles'),
     # la forma de darle like y almacenarlo en nuestro sitio
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 
 ]
+# app_name = 'musicss'
+# urlpatterns = [
+#
+#     url(r'^$', views.index, name='musicsite'),
+#     # aqui se declara algum_id
+#     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detalles'),
+#     # la forma de darle like y almacenarlo en nuestro sitio
+#     url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+#
+# ]
 # esto es un cambio
--- MusicFileFolderSite/MusicSite/views.py
+++ MusicFileFolderSite/MusicSite/views.py
@@ -4,49 +4,62 @@ from django.http import HttpResponse
 # Create your views here.
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album, Song
 from django.http import Http404
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    html = ''
-    # template = loader.get_template('MusicSite/index.html')
-    # django busca por defecto la carpeta template, por loque no hizo falta ponerlo
-    context = {
-        'all_albums': all_albums
-    }
 
-    ###for album in all_albums:
-    ###   url ='/music/' +str(album.id) +'/'
-    ###    html += '<a href="'+url+'">'+album.album_title+'</a><br>'
-    ##return HttpResponse("<h1>Esta es la pagina de musica</h1>")
-    ###return HttpResponse(html)
-    # return HttpResponse(template.render(context,request))
-    return render(request, 'MusicSite/index.html', context)
+class IndexView(generic.ListView):
+    template_name='MusicSite/index.html'
+    context_object_name = 'all_albums'
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name='MusicSite/detail.html'
 
 
-def detail(request, album_id):  # esta funcioin es la que retorna la informacion
-    # de los albumes, realmente es un api lo que estamos
-    # creando creo
-    album = get_object_or_404(Album, pk=album_id, )
-    # try:
-    #     album = Album.objects.get(id=album_id)
-    # except  Album.DoesNotExist:
-    #     raise Http404('Este Album No existe: ID'+str(album_id))
-    return render(request, 'MusicSite/detail.html', {'album': album})
-
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id, )
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'MusicSite/detail.html', {
-            'album': album,
-            'error_message': 'No seleccionaste una cancion correcta',
-        })
-    else:
-        selected_song.is_favorite = not selected_song.is_favorite
-        selected_song.save()
-        return render(request, 'MusicSite/detail.html', {'album': album})
+# def index(request):
+#     all_albums = Album.objects.all()
+#     html = ''
+#     # template = loader.get_template('MusicSite/index.html')
+#     # django busca por defecto la carpeta template, por loque no hizo falta ponerlo
+#     context = {
+#         'all_albums': all_albums
+#     }
+#
+#     ###for album in all_albums:
+#     ###   url ='/music/' +str(album.id) +'/'
+#     ###    html += '<a href="'+url+'">'+album.album_title+'</a><br>'
+#     ##return HttpResponse("<h1>Esta es la pagina de musica</h1>")
+#     ###return HttpResponse(html)
+#     # return HttpResponse(template.render(context,request))
+#     return render(request, 'MusicSite/index.html', context)
+#
+#
+# def detail(request, album_id):  # esta funcioin es la que retorna la informacion
+#     # de los albumes, realmente es un api lo que estamos
+#     # creando creo
+#     album = get_object_or_404(Album, pk=album_id, )
+#     # try:
+#     #     album = Album.objects.get(id=album_id)
+#     # except  Album.DoesNotExist:
+#     #     raise Http404('Este Album No existe: ID'+str(album_id))
+#     return render(request, 'MusicSite/detail.html', {'album': album})
+#
+#
+# def favorite(request, album_id):
+#     album = get_object_or_404(Album, pk=album_id, )
+#     try:
+#         selected_song = album.song_set.get(pk=request.POST['song'])
+#     except(KeyError, Song.DoesNotExist):
+#         return render(request, 'MusicSite/detail.html', {
+#             'album': album,
+#             'error_message': 'No seleccionaste una cancion correcta',
+#         })
+#     else:
+#         selected_song.is_favorite = not selected_song.is_favorite
+#         selected_song.save()
+#         return render(request, 'MusicSite/detail.html', {'album': album})
